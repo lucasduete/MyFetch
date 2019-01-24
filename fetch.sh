a794987401de3ecacf3c9973de5d1ef0b93efd1f
Lucas Duete <lm.duete@gmail.com>
2019-01-24 19:22:50 -0300

    Removed caracter ',' from column Uptime

--- fetch.sh
+++ fetch.sh
@@ -7,7 +7,7 @@ display_info() {
 	d_os=$(uname -o)
 	d_kname=$(cat /proc/sys/kernel/ostype)
 	d_kversion=$(cat /proc/sys/kernel/osrelease)
-	d_uptime=$(uptime | awk '{print $3}')" min"
+	d_uptime=$(uptime | awk '{print $3}' | tr -d ,)" min"
 	d_shell=$(basename $SHELL)
 	d_res=$(xrandr | sed -n '1p' | sed 's/.*current.//g;s/,.*//g;s/ //g')
 	d_deskEnv=$XDG_SESSION_DESKTOP
